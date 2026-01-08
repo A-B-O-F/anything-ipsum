c624c1fb46db637a20a6b628f4fc6f81e9ee21ff
A-B-O-F <159420870+A-B-O-F@users.noreply.github.com>
2026-01-08 11:17:31 +0100

    test: add API endpoint tests

---
+++ tests/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  displayName: 'api-tests',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/**/*.test.js'],
+  coverageDirectory: '../coverage/api-tests',
+  transform: {},
+};
